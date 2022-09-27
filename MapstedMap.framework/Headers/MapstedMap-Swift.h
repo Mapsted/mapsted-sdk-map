ab89bd88dbd584c1577e15ad3369facecf006d82
Parth Bhatt <parth.bhatt@mapsted.com>
2022-09-27 22:07:43 +0530

    Update 4.6.14.1

--- MapstedMap.framework/Headers/MapstedMap-Swift.h
+++ MapstedMap.framework/Headers/MapstedMap-Swift.h
@@ -238,7 +238,7 @@ using UInt = size_t;
 @import UIKit;
 #endif
 
-#import "/Users/Daniel/Projects/Xcode/ios-sdk/ios-sdk-map-core/map/MN-Bridging-Header.h"
+#import "/Users/parth.bhatt/Mapsted/WorkingCopies/MapstedSDK/ios-sdk/ios-sdk-map-core/map/MN-Bridging-Header.h"
 
 #endif
 #pragma clang diagnostic ignored "-Wproperty-attribute-mismatch"
